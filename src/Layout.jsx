07253c8b5223164fc65d1e5fb0986be136db14a0
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-16 06:28:07 -0700

    files done

---
+++ src/Layout.jsx
@@ -0,0 +1,11 @@
+
+
+function Layout() {
+  return (
+    <>
+    <h1>hello layout</h1>
+    </>
+  )
+}
+
+export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Login from './screens/Login.jsx'
+import Register from './screens/Register.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Layout/>,
+    children : [
+      {
+        path : '/',
+        // element : <Todo/>
+      },
+      {
+    path : 'Login',
+    element : <Login/>
+  },
+  {
+    path : 'Register',
+    element : <Register/>
+  }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}>
+    <Layout/>
+  </RouterProvider>
+
 )
